eab0490b1fc61c25faad6a1ab68bff2fd5cf76c2
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-11-20 22:27:42 +0300

    Initial Commit

---
+++ WeSplit1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit1
+//
+//  Created by Furkan Cingöz on 20.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit1/WeSplit1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit1App.swift
+//  WeSplit1
+//
+//  Created by Furkan Cingöz on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
